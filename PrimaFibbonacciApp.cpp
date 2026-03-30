00a286ac9314e47e319a8f921aff125760219b00
risalahlele87-coder <risalahleles87@gmail.com>
2026-03-31 05:29:43 +0700

    penambahan daftar opsi menu 1, 2, dan 0 ke layar

--- PrimaFibbonacciApp.cpp
+++ PrimaFibbonacciApp.cpp
@@ -42,3 +42,8 @@ void inputAngka() {
 // --- Prosedural untuk Menampilkan Menu ---
 void tampilkanMenu() {
     cout << "\n========== MENU UTAMA ==========" << endl;
+    cout << "1. Cek Bilangan Prima" << endl;
+    cout << "2. Cek Bilangan Fibonacci" << endl;
+    cout << "0. Keluar" << endl;
+
+    
